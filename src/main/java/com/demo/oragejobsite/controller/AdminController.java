c29e9dd2d8f7c91c9832608bf683055647dfe81d
Shrey-akan <shereyansjain0904@gmail.com>
2024-05-30 13:32:26 +0530

    as

--- src/main/java/com/demo/oragejobsite/controller/AdminController.java
+++ src/main/java/com/demo/oragejobsite/controller/AdminController.java
@@ -155,12 +155,11 @@ public class AdminController {
     @GetMapping("/fetchadmin")
     public ResponseEntity<List<Admin>> fetchadmin(@RequestParam(required = false) String adminId) {
         try {
-        	List<Admin> admindata = new ArrayList()<>(); ;
-        	if(adminId!=null && !adminId.isEmpty())
-        	{
-        		Optional<Admin> admin = admindao.findByAdminid(adminId);
+            List<Admin> admindata = new ArrayList<>();  // Corrected initialization
+            if (adminId != null && !adminId.isEmpty()) {
+                Optional<Admin> admin = admindao.findByAdminid(adminId);
                 admin.ifPresent(admindata::add);
-        	}else {
+            } else {
                 admindata = admindao.findAll();
             }
             return ResponseEntity.status(HttpStatus.OK).body(admindata);
@@ -169,4 +168,5 @@ public class AdminController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
+
 }
